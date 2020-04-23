f380a972f6e3eec365c14fc64a672d2cc19ee396
amirhk <amirhkarimi@gmail.com>
2020-04-23 17:54:27 +0200

    Memoization now supports default arguments

--- utils.py
+++ utils.py
@@ -1,12 +1,21 @@
-
+import inspect
 
 # See https://www.python-course.eu/python3_memoization.php
-# TODO: add support for keyword arguments: https://stackoverflow.com/a/37858058
 class Memoize:
+
   def __init__(self, fn):
     self.fn = fn
     self.memo = {}
-  def __call__(self, *args):
+  def __call__(self, *args, **kwargs):
+
+    # to support default args: https://docs.python.org/3.3/library/inspect.html
+    sig = inspect.signature(self.fn)
+    ba = sig.bind(*args)
+    for param in sig.parameters.values():
+      if param.name not in ba.arguments:
+        ba.arguments[param.name] = param.default
+    args = ba.args
+
     if args not in self.memo:
       self.memo[args] = self.fn(*args)
     return self.memo[args]
